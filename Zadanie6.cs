a00f0c111e7e06f5545792108266142ab6528415
WhiteMirac1e <115178943+WhiteMirac1e@users.noreply.github.com>
2022-10-18 14:12:38 +0300

    Add files via upload

---
+++ Zadanie6.cs
@@ -0,0 +1,18 @@
+﻿string Ev(int num)
+    {
+        string result = "Нечетное";
+        if (num % 2 == 0 )
+            {
+                result = "Четное";
+                return result;
+            }
+            else
+                {
+                    return result;
+                }
+    }
+
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Ev(num));
